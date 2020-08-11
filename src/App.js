d62c8a96293e43f15b0e68411171137362c371e4
putkara <natia.meskhi.natia@gmail.com>
2020-08-11 09:23:11 +0200

    natia sol

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+import "./App.scss"
+
+
+export default class App extends React.Component {
+render(){
+  return(
+    <div>
+      
+    <h1>Hello world</h1>
+    
+    </div>
+    
+  )
+}
+
+}
